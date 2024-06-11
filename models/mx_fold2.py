a4b0c9a15777a1fbcb7505c8ec2490dcd0beeb13
KraszewskiK <Kraszewski.K@proton.me>
2024-06-11 13:37:44 +0200

    Update mx_fold2.py
    Added docstrings and small refactoring.

--- models/mx_fold2.py
+++ models/mx_fold2.py
@@ -2,6 +2,11 @@ import subprocess
 
 
 def run_mxfold2(input_file):
+    """
+    Run mxfold2 on the input file and return the results as a dictionary.
+    :param input_file: A file containing the sequences to predict the secondary structure for.
+    :return: A dictionary containing the results in the format {header: (sequence, dot_bracket)}.
+    """
     # Construct the command
     command = ['mxfold2', 'predict', input_file]
 
@@ -9,9 +14,9 @@ def run_mxfold2(input_file):
     result = subprocess.run(command, capture_output=True, text=True)
 
     mx_fold_res = result.stdout.split(">")[1:]
-    temp_dict = {}
+    res_dict = {}
     for elt in mx_fold_res:
         content = elt.split("\n")
-        temp_dict[content[0]] = (content[1].strip(), content[2].split()[0].strip())
+        res_dict[content[0]] = (content[1].strip(), content[2].split()[0].strip())
 
-    return temp_dict
+    return res_dict
